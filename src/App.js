43dce9d381c902f2e9a137ad5d4d818cce2e682f
LePhuThanh <lpthanh710@gmail.com>
2023-03-04 16:25:09 +0700

    update code

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello World! created by Phu Thanh
         </p>
         <a
           className="App-link"
@@ -21,5 +21,5 @@ function App() {
     </div>
   );
 }
-
+//asdasdasd
 export default App;
